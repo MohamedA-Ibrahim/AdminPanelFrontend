ca7e4f860e5499ba4eca6187f32021808d5adb55
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-06-30 10:29:32 +0300

    Generate components for users and user details

---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-details',
+  imports: [],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.scss'
+})
+
+export class UserDetailsComponent {
+
+}
