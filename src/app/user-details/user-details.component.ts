aa2b8adb6cfde1b6a1a347c84a5efd5b9186ba75
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-06-30 10:44:28 +0300

    Add placeholder content to User details and users

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   imports: [],
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
+  styleUrl: './user-details.component.scss',
 })
+export class UserDetailsComponent implements OnInit {
+  userId: string | null = null;
 
-export class UserDetailsComponent {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.userId = params['id'];
+    });
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent {
-
+  users = [
+    { id: 1, name: 'User 1' },
+    { id: 2, name: 'User 2' },
+    { id: 3, name: 'User 3' },
+  ];
 }
