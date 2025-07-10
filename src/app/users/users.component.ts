0af29ad71ba4fb2002da22955fc8479934d52926
Mohamed <mohamed.ib1751@gmail.com>
2025-07-10 10:49:22 +0300

    Implement Delete user with confirmation dialog

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { User } from '../User';
 import { UsersService } from '../users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-users',
@@ -28,7 +29,27 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  deleteUser(){
-    
+  deleteUser(id: string) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you really want to delete this user?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.usersService.removeUser(id).subscribe({
+          next: () => {
+            this.users = this.users.filter(user => user.id !== id);
+            Swal.fire('Deleted!', 'User has been deleted.', 'success');
+          },
+          error: (err) => {
+            Swal.fire('Error', 'Failed to delete user: ' + (err.error), 'error');
+          }
+        });
+      }
+    });
   }
 }
