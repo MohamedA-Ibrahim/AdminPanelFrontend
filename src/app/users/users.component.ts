5782c667ff12615708800dae9d9d34974afc242d
Mohamed <mohamed.ib1751@gmail.com>
2025-07-16 01:42:54 +0300

    Replace user list with a table

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import { User } from '../User';
 import { UsersService } from '../users.service';
 import Swal from 'sweetalert2';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-users',
-  imports: [RouterLink],
+  imports: [RouterLink, MatTableModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
@@ -37,19 +38,21 @@ export class UsersComponent implements OnInit {
       showCancelButton: true,
       confirmButtonColor: '#d33',
       cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonText: 'Yes, delete it!',
     }).then((result) => {
       if (result.isConfirmed) {
         this.usersService.removeUser(id).subscribe({
           next: () => {
-            this.users = this.users.filter(user => user.id !== id);
+            this.users = this.users.filter((user) => user.id !== id);
             Swal.fire('Deleted!', 'User has been deleted.', 'success');
           },
           error: (err) => {
-            Swal.fire('Error', 'Failed to delete user: ' + (err.error), 'error');
-          }
+            Swal.fire('Error', 'Failed to delete user: ' + err.error, 'error');
+          },
         });
       }
     });
   }
+
+  columnsToDisplay = ['firstName', 'lastName', 'email', 'phone','action'];
 }
