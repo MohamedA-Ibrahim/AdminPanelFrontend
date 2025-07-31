58451c754f1546ecb1cbaf30a1580ee4e5ed1738
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-07-31 18:22:43 +0300

    Add sorting to users list page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,18 +1,31 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { User } from '../User';
 import { UsersService } from '../users.service';
 import Swal from 'sweetalert2';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+
 @Component({
   selector: 'app-users',
-  imports: [RouterLink, MatTableModule, MatProgressSpinnerModule],
+  imports: [
+    RouterLink,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit {
   users: User[] = [];
   isLoading = false;
   isError = false;
@@ -20,16 +33,27 @@ export class UsersComponent implements OnInit {
   private usersService = inject(UsersService);
   private snackBar = inject(MatSnackBar);
 
+  dataSource = new MatTableDataSource<User>();
+  columnsToDisplay = ['firstName', 'lastName', 'email', 'phone', 'action'];
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+  }
+
   ngOnInit() {
     this.isLoading = true;
 
     this.usersService.getUsers().subscribe({
       next: (resData) => {
         this.users = resData;
+        this.dataSource.data = this.users;
+        this.dataSource.sort = this.sort;
+        console.log(this.sort);
       },
       error: (err) => {
         console.error(err.message);
-        
+
         this.isError = true;
         this.snackBar.open('Failed to get users due to server error', 'ok', {
           duration: 7000,
@@ -66,5 +90,6 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  columnsToDisplay = ['firstName', 'lastName', 'email', 'phone', 'action'];
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {}
 }
