5f8f17247912753d8ea698e1005c6af623f6bb8f
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-10-03 14:19:28 +0300

    Separate html and style of grpc-dialog

--- src/app/grpc-dialog/grpc-dialog.component.ts
+++ src/app/grpc-dialog/grpc-dialog.component.ts
@@ -1,36 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { UserGrpcService } from '../grpc/UserGrpcService';
 
 
 @Component({
   selector: 'app-grpc-dialog',
   standalone: true,
   imports: [MatDialogModule, MatButtonModule],
-  template: `
-    <h2 mat-dialog-title>Try gRPC</h2>
-    <mat-dialog-content>
-      <p>GRPC.</p>
-    </mat-dialog-content>
-    <mat-dialog-actions align="end">
-      <button mat-button mat-dialog-close>Close</button>
-    </mat-dialog-actions>
-  `,
-  styles: [
-    `
-      :host {
-        display: block;
-        padding: 20px;
-        min-width: 300px;
-      }
-    `,
-  ],
+  templateUrl: './grpc-dialog.component.html',
+  styleUrls: ['./grpc-dialog.component.scss']
 })
-export class GrpcDialogComponent implements OnInit {
-  private readonly userGrpcService = inject(UserGrpcService);
+export class GrpcDialogComponent {
 
-  ngOnInit() {
-    this.userGrpcService.getUserById('3F6A38E1-F7D1-44D4-8DA2-0871F3ABEECE');
-  }
 }
